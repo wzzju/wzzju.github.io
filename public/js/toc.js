7ccb305bf5966fa9a3092fe41175e4f2c7dca165
Zhen Wang <wangzhen31@baidu.com>
2022-03-12 19:22:13 +0800

    Support the floating menu.

---
+++ public/js/toc.js
@@ -0,0 +1,43 @@
+// toggle side catalog
+$(".catalog-toggle").click((function (e) {
+  e.preventDefault();
+  $('.side-catalog').toggleClass("fold")
+}));
+
+// Navigation Scripts to Show Header on Scroll-Up
+jQuery(document).ready(function ($) {
+  var MQL = 1170;
+
+  // primary navigation slide-in effect
+  if ($(window).width() > MQL) {
+      var bannerHeight = $('.wrap').height();
+      $(window).on('scroll', {
+              previousTop: 0
+          },
+          function () {
+              var currentTop = $(window).scrollTop(),
+                  $catalog = $('.side-catalog');
+              this.previousTop = currentTop;
+              // adjust the appearance of side-catalog
+              $catalog.show()
+              if (currentTop > (bannerHeight + 41)) {
+                  $catalog.addClass('fixed')
+              } else {
+                  $catalog.removeClass('fixed')
+              }
+          });
+  }
+});
+
+$(document).ready(function () {
+  tocbot.init({
+      // Where to render the table of contents.
+      tocSelector: '.catalog-body',
+      // Where to grab the headings to build the table of contents.
+      contentSelector: '.post-container',
+      // Which headings to grab inside of the contentSelector element.
+      headingSelector: 'h1, h2, h3, h4, h5',
+      // For headings inside relative or absolute positioned containers within content.
+      hasInnerContainers: true,
+  });
+});
